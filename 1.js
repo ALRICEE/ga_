fda56c2789b0433d7e645c24679d5c714fd44ebc
ALRICEE <alivia.rice.pht@gmail.com>
2024-07-03 16:30:16 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,1 @@
-const links = ["https://matchpartners.life/?u=z34zyqu&o=th08x3z&m=1&t=schmb",
-"https://hugebonusfinder.life/?u=z34zyqu&o=thw8x3z&m=1&t=schmb"
-      ];
-      const randomLink = links[Math.floor(Math.random() * links.length)];
-      window.location.href = randomLink;
+//
